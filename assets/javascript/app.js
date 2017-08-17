bcd270f933d9bf2dfb2b356a671a4aefcb134415
jasonjoachim <joachim.jason@gmail.com>
2017-08-17 01:04:24 -0500

    finished the questions, added timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,6 +4,19 @@ var wrongAnswers=0;
 
 var noAnswers=0;
 
+var timeLeft=120;
+var intervalId=0;
+
+intervalId= setInterval(function(){
+    if(timeLeft<=0){
+        console.log("Time's UP");
+        clearInterval(intervalId=0);
+        timeLeft=120;
+    }
+    $("#timeLeft").html("You Have "+timeLeft+" Seconds Left")
+    // console.log(timeLeft+"Seconds Left"+timeLeft--)
+
+},1000)
 
 $(document).ready(function(){
 
@@ -13,10 +26,7 @@ $(document).ready(function(){
         wrongAnswers = 0;
         noAnswers = 0;
 
-        console.log("TEST");
-    
         var q1Answer = $("input[name=question1]:checked").val();
-        console.log(q1Answer);
         if (q1Answer==="true"){
             correctAnswers++;
         } else if(q1Answer=="false"){
@@ -24,27 +34,82 @@ $(document).ready(function(){
         } else {
             noAnswers++;
         }
-        console.log(correctAnswers,wrongAnswers,noAnswers)
-
-    });
-$("#submit").click(function (){
-        correctAnswers = 0;
-        wrongAnswers = 0;
-        noAnswers = 0;
-        
-        console.log("TEST");
-    
         var q2Answer = $("input[name=question2]:checked").val();
-        console.log(q2Answer);
         if (q2Answer==="false"){
             correctAnswers++;
         } else if(q2Answer=="true"){
             wrongAnswers++;
         } else {
             noAnswers++;
+        }  
+        var q3Answer = $("input[name=question3]:checked").val();
+        if (q3Answer==="false"){
+            correctAnswers++;
+        } else if(q3Answer=="true"){
+            wrongAnswers++;
+        } else {
+            noAnswers++;
+        }
+        var q4Answer = $("input[name=question4]:checked").val();
+        if (q4Answer==="true"){
+            correctAnswers++;
+        } else {
+            wrongAnswers++;}
+        var q5Answer = $("input[name=question5]:checked").val();
+        if (q5Answer==="true"){
+            correctAnswers++;
+        } else if(q5Answer=="false"){
+            wrongAnswers++;
+        } else {
+            noAnswers++;
+        }
+        var q6Answer = $("input[name=question6]:checked").val();
+        if (q6Answer==="true"){
+            correctAnswers++;
+        } else if(q6Answer=="false"){
+            wrongAnswers++;
+        } else {
+            noAnswers++;
+        }
+        var q7Answer = $("input[name=question7]:checked").val();
+        if (q7Answer==="true"){
+            correctAnswers++;
+        } else if(q7Answer=="false"){
+            wrongAnswers++;
+        } else {
+            noAnswers++;
+        }
+        var q8Answer = $("input[name=question8]:checked").val();
+        if (q8Answer==="false"){
+            correctAnswers++;
+        } else if(q8Answer=="true"){
+            wrongAnswers++;
+        } else {
+            noAnswers++;
+        }
+        var q9Answer = $("input[name=question9]:checked").val();
+        if (q9Answer==="false"){
+            correctAnswers++;
+        } else if(q9Answer=="true"){
+            wrongAnswers++;
+        } else {
+            noAnswers++;
+        }
+        var q10Answer = $("input[name=question10]:checked").val();
+        if (q10Answer==="true"){
+            correctAnswers++;
+        } else if(q10Answer=="false"){
+            wrongAnswers++;
+        } else {
+            noAnswers++;
         }
         console.log(correctAnswers,wrongAnswers,noAnswers)
     });
+
+
+
+
+
 
 
 
